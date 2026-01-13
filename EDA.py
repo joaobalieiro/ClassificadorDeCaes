3bb5f17af4deda54e8b5d6cc15fdbfb405aa466b
joaobalieiro <joaobalieiro@usp.br>
2026-01-13 09:34:49 -0300

    selecao_raca

--- EDA.py
+++ EDA.py
@@ -16,3 +16,16 @@ imagens = os.listdir(path)
 print('{} folders in img_folder'.format(len(imagens)))
 print('\n'.join(imagens))
 
+# recupera as imagens de dobermans
+Doberman_folder = os.path.join(path,'n02107142-Doberman')
+
+Doberman_images = os.listdir(Doberman_folder)
+print(f"{len(Doberman_images)} images in Doberman_folder")
+
+os.chdir(Doberman_folder)
+
+im = Image.open(Doberman_images[12],'r')
+plt.imshow(im)
+plt.axis("off")
+plt.show()
+print(im.format, im.size, im.mode)
